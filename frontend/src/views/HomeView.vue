82776a771d314277125bc0413de9fe43550b38de
Ritsuki Irabu <sekaowa.ritsuki@gmail.com>
2026-06-27 11:06:47 +0900

    feat: set up vue pwa frontend

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useAppStore } from '../stores/app'
+
+const appStore = useAppStore()
+
+const setupItems = [
+  'Vue Router で画面遷移を準備',
+  'Pinia で状態管理を準備',
+  'Axios でLaravel API通信を準備',
+  'PWA設定とアイコンを追加',
+]
+</script>
+
+<template>
+  <main class="home">
+    <p class="eyebrow">Issue #9 Vue.js + PWA 環境構築</p>
+    <h1>{{ appStore.appName }}</h1>
+    <p class="lead">ログイン画面やマイリスト画面を作るための土台が動いています。</p>
+
+    <ul class="setup-list">
+      <li v-for="item in setupItems" :key="item">
+        {{ item }}
+      </li>
+    </ul>
+  </main>
+</template>
